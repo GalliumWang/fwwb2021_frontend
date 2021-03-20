d82c8abb65d83ed6fa272eb787bb2a6393846d04
Gallium Wang <galliumwang199@gmail.com>
2021-03-20 18:36:13 +0800

    add route and district display control

--- subway_embeded_in_map/control_panel.js
+++ subway_embeded_in_map/control_panel.js
@@ -11,3 +11,51 @@ function downloadURI(uri, name) {
 function capture_screen(){ 
 downloadURI(map.getCanvas().toDataURL(),'screen_capture.png'); 
 }
+
+function get_route_data(value){
+    value=parseInt(value);
+    var origin=JSON.parse(JSON.stringify(lineData));
+    switch (value) {
+        case -1:
+            break;
+        case 0:
+            origin.features=[];
+            break;
+        default:
+            origin.features=[origin.features[value-1],];
+            break;
+    }
+    console.log(JSON.stringify(origin));
+    return origin;
+}
+
+function get_district_data(value){
+    value=parseInt(value);
+    var origin=JSON.parse(JSON.stringify(district_data));
+    console.log(JSON.stringify(origin));
+    switch (value) {
+        case -1:
+            break;
+        case 0:
+            origin.features=[];
+            break;
+        default:
+            origin.features=[origin.features[value-1],];
+            break;
+    }
+    console.log(JSON.stringify(origin));
+    return origin;
+}
+
+params.addEventListener('change', function (e) {
+    if (e.target.name === 'route') {
+        var value = e.target.value;
+        var data=get_route_data(value);
+        map.getSource('trips').setData(data);
+    } 
+    else if (e.target.name === 'district') {
+        var value = e.target.value;
+        var data=get_district_data(value);
+        map.getSource('districtLayer').setData(data);
+    }
+});
